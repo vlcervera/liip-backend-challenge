275af1610ac88bd61128565f3fb782f3bec70849
Victor Luis Cervera <victor.luis@macbook-pro-victor.local>
2020-06-20 19:43:32 +0200

    fix() - Translation comment

--- src/main/java/org/vlcervera/beer/domain/model/translation/Translation.java
+++ src/main/java/org/vlcervera/beer/domain/model/translation/Translation.java
@@ -8,9 +8,8 @@ import java.util.Optional;
  * Common behaviour required in all Translation implementations.
  * <p>
  * Given a number the translation must return a string with the defined number conversion.
- * In case of the number doesn't meet the condition the value retrieved ins NO_TRANSLATION_VALUE
+ * In case of the number doesn't meet the condition the value retrieved is an empty optional
  */
 public interface Translation {
-
     Optional<String> translate(int numberToTranslate);
 }
