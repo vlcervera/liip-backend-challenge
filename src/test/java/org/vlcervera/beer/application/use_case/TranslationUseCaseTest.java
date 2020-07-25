a65988cfe1f48a7d1bab936693eb781a4f735e85
Victor Luis Cervera <victor.luis@macbook-pro-victor.local>
2020-07-25 21:40:47 +0200

    fix() - bad import in Assertions.assertThrows

--- src/test/java/org/vlcervera/beer/application/use_case/TranslationUseCaseTest.java
+++ src/test/java/org/vlcervera/beer/application/use_case/TranslationUseCaseTest.java
@@ -1,16 +1,17 @@
 package org.vlcervera.beer.application.use_case;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.vlcervera.beer.domain.exception.NumberToStartIsGreaterThanLimitException;
-import org.vlcervera.beer.domain.port.TranslationSaveRepositoryPort;
 import org.vlcervera.beer.domain.model.translation.Translations;
+import org.vlcervera.beer.domain.port.TranslationSaveRepositoryPort;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,12 +44,12 @@ public class TranslationUseCaseTest {
         verify(translationSaveRepositoryPort, times(1))
                 .save(argThat(list -> list.size() == numbersToTranslate));
 
-        Assertions.assertThat(numbersTranslated).hasSize(numbersToTranslate);
+        assertThat(numbersTranslated).hasSize(numbersToTranslate);
     }
 
     @Test
     public void shouldThrowExceptionWhenNumberToStartIsGreaterThanLimit() {
-        org.junit.jupiter.api.Assertions.assertThrows(NumberToStartIsGreaterThanLimitException.class, () -> {
+        assertThrows(NumberToStartIsGreaterThanLimitException.class, () -> {
             //GIVEN
             int limit         = 100;
             int numberToStart = 400;
